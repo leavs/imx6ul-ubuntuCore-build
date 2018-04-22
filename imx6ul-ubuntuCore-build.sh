609ce92209a046d1f2fc9d618a3dbd77c536b2be
leavs <leavs_o@126.com>
2018-04-21 20:06:07 -0700

    repair the gadget error and change kernel to devmode

--- imx6ul-ubuntuCore-build.sh
+++ imx6ul-ubuntuCore-build.sh
@@ -3,14 +3,14 @@
 # kernel-snap
 echo "build imx6ul-eisd-kernel ..."
 cd imx6ul-eisd-kernel
-snapcraft snap --target-arch armhf
+snapcraft --target-arch=armhf
 cd ..
-echo "build imx6ul-eisd-kernel done!!"
+echo "build imx6uleisd-kernel done!!"
 
 # gadget-snap
 echo "build imx6ul-eisd-gadget"
 cd imx6ul-eisd-gadget
-snapcraft snap
+snapcraft
 cd ..
 echo "build imx6ul-eisd-gadget done!!"
 
@@ -18,8 +18,8 @@ echo "build imx6ul-eisd-gadget done!!"
 echo "build ubuntu core image ..."
 cd ubuntu-core-image
 cp ../mx6ul-eisd-gadget/imx6ul-eisd-gadget_16.01-1_armhf.snap .
-cp ../imx6ul-eisd-kernel/imx6ul-eisd-kernel_3.14.52_armhf.snap .
+cp ../imx6ul-eisd-kernel/imx6uleisd-kernel_3.14.52_armhf.snap .
 cp ../model-assertion/imx6ul-eisd.model .
 
-sudo ubuntu-image -c beta --image-size 4G --extra-snaps imx6ul-eisd-gadget_16.01-1_armhf.snap --extra-snaps imx6ul-eisd-kernel_3.14.52_armhf.snap -O imx6ul-eisd-20180421 imx6ul-eisd.model
+ubuntu-image -c beta --image-size 4G --extra-snaps imx6ul-eisd-gadget_16.01-1_armhf.snap --extra-snaps imx6uleisd-kernel_3.14.52_armhf.snap -O imx6ul-eisd-20180422 imx6ul-eisd.model
 echo "build ubuntu core image done!!"
