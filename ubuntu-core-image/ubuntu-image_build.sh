9c0133175e3e48904cc594047b9159d093f0152a
leavs <leavs_o@126.com>
2018-04-23 17:07:37 +0800

    modify gadget structure

---
+++ ubuntu-core-image/ubuntu-image_build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ubuntu-image -c beta --image-size 1G --extra-snaps imx6ul-eisd-gadget_16.01-1_armhf.snap --extra-snaps imx6uleisd-kernel_3.14.52_armhf.snap -O imx6ul-eisd-20180422 imx6ul-eisd.model
